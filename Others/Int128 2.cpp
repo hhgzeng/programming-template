bbb6bb995e89170dd31c0b906c5eea7743efa25d
jing zeng <jingzeng@Mac.lan>
2025-01-23 22:51:41 +0800

    add file

---
+++ Others/Int128 2.cpp
@@ -0,0 +1,12 @@
+std::ostream &operator<<(std::ostream &os, i128 n) {
+    if (n == 0) {
+        return os << 0;
+    }
+    std::string s;
+    while (n > 0) {
+        s += char('0' + n % 10);
+        n /= 10;
+    }
+    std::reverse(s.begin(), s.end());
+    return os << s;
+}
